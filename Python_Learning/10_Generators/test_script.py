fa9fe64fa937f8a30b62bdcbae05a0f5f9d1864c
Starboy <samskywalker001@gmail.com>
2022-03-28 16:13:45 +0530

    Update

--- Python_Learning/10_Generators/test_script.py
+++ Python_Learning/10_Generators/test_script.py
@@ -1,14 +1,2 @@
+# It's just a test script...
 
-def fibonacci_num(num):
-    a = 0
-    b = 1
-    fibonacci_nums = []
-    for i in range(num):
-        fibonacci_nums.append(a)
-        temp = a    
-        a = b
-        b = temp + b
-
-
-for x in fibonacci_num(21):
-    print(x)
